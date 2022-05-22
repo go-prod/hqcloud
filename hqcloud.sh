8645817b156aaaae23e10b56101970ada30701e2
syswu <wuheng@iscas.ac.cn>
2022-05-22 06:55:41 +0000

    init ntpd

--- hqcloud.sh
+++ hqcloud.sh
@@ -7,6 +7,8 @@
 
 nic="eth0"
 
+ntpd="127.0.0.1"
+
 ports=""
 #ports="179/tcp 5138/tcp 2379-2380/tcp 2375/tcp 8443/tcp 5000/tcp 6443/tcp 443/tcp 8000/tcp 9094/udp 8083/tcp 8089/tcp 8090/tcp 9090-9094/tcp 80/tcp 8080/tcp 9100/tcp 9093/tcp 53/tcp 53/udp 9253/tcp 9153/tcp 10249-10256/tcp 20000-40000/tcp "
 
@@ -28,7 +30,11 @@ function set-zone()
 function set-ntpd
 {
   yum install ntp -y
-  
+  output=$(cat /etc/crontab | grep $ntpd)
+  if [[ -z $output ]]
+  then
+     echo "0 * * * * /usr/sbin/ntpdate $ntpd  && /sbin/hwclock --systohc > /dev/null 2>&1" >> /etc/crontab 
+  fi
 }
 
 function set-charset
